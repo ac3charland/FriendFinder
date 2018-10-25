bf881402141364c130f8459a7d1cfdab3852cd54
Alex Charland <ac3charland@gmail.com>
2018-10-25 18:15:20 -0500

    Nearly done, now testing for final bugs.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,31 @@
-[
+var friends = [
     {
         name: "John Cleese",
         photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/John_Cleese_at_1989_Oscars.jpg/220px-John_Cleese_at_1989_Oscars.jpg",
-        answers: [5,5,5,5,5,5,5,5,5,5]
+        answers: [2, 5, 5, 1, 3, 2, 1, 5, 1, 3]
+    },
+    {
+        name: "Michael Palin",
+        photo: "https://i.dailymail.co.uk/i/pix/2009/09/14/article-0-0009000300000258-182_306x423.jpg",
+        answers: [3, 5, 1, 5, 3, 2, 4, 1, 5, 5]
+    },
+    {
+        name: "Graham Chapman",
+        photo: "https://upload.wikimedia.org/wikipedia/en/f/f7/Graham_Chapman_Portrait.png",
+        answers: [5, 2, 4, 1, 1, 5, 3, 1, 1, 3]
+    },
+    {
+        name: "Terry Jones",
+        photo: "https://images.gr-assets.com/authors/1217440293p5/209.jpg",
+        answers: [1, 4, 4, 1, 3, 2, 4, 1, 1, 2]
+    },
+    {
+        name: "Terry Gilliam",
+        photo: "http://static.bafta.org/images/originals/terry-gilliam-archive-bbc-2946.jpg",
+        answers: [5, 3, 3, 1, 3, 3, 3, 3, 1, 3]
     }
 ]
+
+module.exports = {
+    array: friends
+}
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,21 +1,21 @@
+var friends = require("../data/friends");
+
 
 module.exports = function(app) {
     app.post("/api/friends", function (req, res) {
         // req.body hosts is equal to the JSON post sent from the user
         // This works because of our body-parser middleware
         var newfriend = req.body;
-
-        // Using a RegEx Pattern to remove spaces from newCharacter
-        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+        console.log("Adding friend...");
 
         console.log(newfriend);
 
-        friends.push(newfriend);
+        friends.array.push(newfriend);
 
-        res.json(newcharacter);
+        res.end();
     });
 
     app.get("/api/friends", function (req, res) {
-        return res.json(friends);
+        return res.json(friends.array);
     });
 }
